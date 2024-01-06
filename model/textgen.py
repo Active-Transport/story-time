88e5a75984dd9791156103c982459fd5d8de86ce
Aniket Mishra <mishra2@ualberta.ca>
2024-01-06 16:54:23 -0700

    Update textgen.py

--- model/textgen.py
+++ model/textgen.py
@@ -14,21 +14,29 @@ prompt_input = "baby Yoda"
 # top_p - is the probability of the next token being in the top p tokens, 
 # max_length - is the maximum length of the output, 
 # repetition_penalty - is the penalty for repeating tokens
+import replicate
+
+# Prompts
+pre_prompt = "Tell a dark and gruesome story about"
+prompt_input = "baby Yoda"
+
+# Generate LLM response
 output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', # LLM model
-                        input={"prompt": f"{pre_prompt} {prompt_input}. Divide it into three paragraphs. Output Format should be P1: write the generated paragraph one here; P2: write the generated paragraph two here; P3: Write the generated paragraph 3 here;", # Prompts
-                        "temperature":0.75, "top_p":0.9, "max_length":1028, "repetition_penalty":1})  # Model parameters
-     
+                        input={"prompt": f"{pre_prompt} {prompt_input}. Divide it into three paragraphs.", # Prompts
+                        "temperature":0.80, "top_p":0.9, "max_length":1028, "repetition_penalty":1})  # Model parameters
+   
 # Converting the output to string
 full_response = ""
 for item in output:
   full_response += item
 
-# Regex to extract the three paragraphs individually
-p1 = re.search(r'P1:(.*)P2:', full_response, re.DOTALL)
-p2 = re.search(r'P2:(.*)P3:', full_response, re.DOTALL)
-p3 = re.search(r'P3:(.*)', full_response, re.DOTALL)
+# print(full_response)
+# print("\n")
+# Splitting the response into paragraphs based on the spaces and selecting the three entries biggest size
+paragraphs = full_response.split("\n")
+paragraphs.sort(key=len, reverse=True)
 
-# Printing the three paragraphs
-print("Paragraph 1: ", p1.group(1))
-print("Paragraph 2: ", p2.group(1))
-print("Paragraph 3: ", p3.group(1))
+# Print the paragraphs
+print("Paragraph 1: ", paragraphs[0])
+print("Paragraph 2: ", paragraphs[1])
+print("Paragraph 3: ", paragraphs[2])
