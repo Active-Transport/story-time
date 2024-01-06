2ee263144c532466e17f76fd9a01477eceb64893
yuihan <98428883+antarc0y@users.noreply.github.com>
2024-01-06 13:20:02 -0700

    added a bunch of base component files, updated react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavHeader from "./Components/NavHeader";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <NavHeader />
+        {/* <Hero></Hero>
+        <LandingContent></LandingContent> */}
+        <Routes>
+          <Route path="/StoryTime/" />
+          <Route path="ActiveTransport/storytime" />
+        </Routes>
+        {/* <Footer /> */}
+      </Router>
+    </>
   );
 }
+
 
 export default App;
