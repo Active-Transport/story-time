3e9008ecdec285b19ec60f7b1c2102c39c756ed0
trinitythai <108836417+trinitythai@users.noreply.github.com>
2024-01-06 23:07:12 -0700

    page 3 in the works

---
+++ frontend/src/Components/story.js
@@ -0,0 +1,30 @@
+import "./styles/story.css"
+
+const Story = () => {
+    return ( 
+    
+    <div className="Story-container">
+        <div>
+            <img src="/images/noimage.gif" />
+            <p>text</p>
+        </div> 
+
+        <div>
+            <img src="/images/noimage.gif" />
+            <p>text</p>
+        </div>
+
+        <div>
+            <img src="/images/noimage.gif" />
+            <p>text</p>
+        </div> 
+    </div> );
+}
+
+<div className="text">
+    font-family
+    font-size
+    colour
+</div>
+
+export default Story;
