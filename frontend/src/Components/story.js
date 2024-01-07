69daf9323e6d38d252050ae103c3150b7c534991
Tyler Beach <tabeach@ualberta.ca>
2024-01-07 07:32:19 -0700

    update story.js

--- frontend/src/Components/story.js
+++ frontend/src/Components/story.js
@@ -6,7 +6,9 @@ import React, { useState, useEffect } from 'react';
 const Story = () => {
     const [inputText, setInputText] = useState('');
     const [storyData, setStoryData] = useState('');
+    const [audio, setAudio] = useState('');
 
+    // STORY GENERATION AND FORM
     const handleSubmit = async (event) => {
         event.preventDefault();
         const requestOptions = {
@@ -20,17 +22,44 @@ const Story = () => {
         const data = await response.json();
         setStoryData(data);
         console.log(data);
-        // console.log(data["0"]);
     };
+
+
+
     
+    // AUDIO GENERATION 
+    // const handAudioClick = async (event) => {
+    //     event.preventDefault();
+    //     const requestOptions = {
+    //         method: 'POST',
+    //         headers: { 'Content-Type': 'application/json' },
+    //         body: JSON.stringify({ storyData })
+    //     };
 
+    //     // Replace URL with your Flask server endpoint
+    //     const response = await fetch('http://127.0.0.1:5000/audio', requestOptions);
+    //     const data = await response.json();
+    //     setAudio(data);
+    //     console.log(data);
+    // };
+    
+    const handleAudioOne = () => {
+        const audio = new Audio("/icons/audio1.mp3")
+        audio.play();
+    };
+     
+    const handleAudioTwo = () => {
+        const audio = new Audio("/frontend/audio1.mp3");
+        audio.play();
+    }
+    
+     
+    const handleAudioThree = () => {
+        const audio = new Audio("/backend/audio1.mp3");
+        audio.play();
+    }
 
-    // AUDIO THINGS
-
-
-
-
-    const audioClip = { sound: storyData.audio, label: "upper cut" };
+    const audioClip = { sound: "http://soundbible.com/mp3/Upper Cut-SoundBible.com-1272257235.mp3", label: "upper cut" };
     const playSound = (src) => {
         const sound = new Howl({
             src,
@@ -55,6 +84,7 @@ const Story = () => {
                     type="submit">Generate Story</button>
                 </form>
             </div>
+            <button onClick={() => handleAudioOne()}>Audio Button</button>
 
             <div>
             {storyData && (
@@ -65,7 +95,7 @@ const Story = () => {
                     <div className="story-part">
                         <div className="audio-div">
                             <h2>Listen along to the story!</h2>
-                            <button onClick={() => playSound(audioClip.sound)}>Audio Button</button>
+                            <button onClick={() => handleAudioOne()}>Audio Button</button>
                         </div>
                         <img src="/icons/logo_cream.jpg" alt="https://placehold.co/400" />
                         <p>{storyData.paragraph1}</p>
@@ -74,7 +104,7 @@ const Story = () => {
                     <div className="story-part">
                         <div className="audio-div">
                             <h2>Listen along to the story!</h2>
-                            <button onClick={() => playSound(audioClip.sound)}>Audio Button</button>
+                            <button onClick={() => handleAudioTwo()}>Audio Button</button>
                         </div>
                         <img src="/icons/logo_cream.jpg" alt="https://placehold.co/400" />
                         <p>{storyData.paragraph2}</p>
@@ -83,7 +113,7 @@ const Story = () => {
                     <div className="story-part">
                         <div className="audio-div">
                             <h2>Listen along to the story!</h2>
-                            <button onClick={() => playSound(audioClip.sound)}>Audio Button</button>
+                            <button onClick={() => handleAudioThree()}>Audio Button</button>
                         </div>
                         <img src="/icons/logo_cream.jpg" alt="https://placehold.co/400" />
                         <p>{storyData.paragraph3}</p>
